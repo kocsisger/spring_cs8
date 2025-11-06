060984e25de735c63a74b2fb05942b8af7161375
Kocsis Gergely <kocsisger@example.com>
2025-11-06 09:40:34 +0100

    Final

--- src/main/java/hu/unideb/inf/SpringDataFX/SpringDataFxApplication.java
+++ src/main/java/hu/unideb/inf/SpringDataFX/SpringDataFxApplication.java
@@ -1,13 +1,34 @@
 package hu.unideb.inf.SpringDataFX;
 
+import hu.unideb.inf.SpringDataFX.model.Person;
+import hu.unideb.inf.SpringDataFX.model.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+
 @SpringBootApplication
-public class SpringDataFxApplication {
+public class SpringDataFxApplication implements CommandLineRunner {
+	@Autowired
+	PersonRepository personRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataFxApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Person p = new Person();
+		p.setName("Jóska");
+		p.setDateOfBirth(LocalDate.of(2017,11,6));
+
+		Person p2 = Person.builder()
+				.name("Mária")
+				.dateOfBirth(LocalDate.of(2017,3,12))
+				.build();
+
+		personRepository.save(p);
+	}
 }
