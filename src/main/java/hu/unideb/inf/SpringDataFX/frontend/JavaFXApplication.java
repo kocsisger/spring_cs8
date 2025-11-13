6261343e247cfda56c2caff00c2766f539e0861e
Kocsis Gergely <kocsisger@example.com>
2025-11-13 08:23:47 +0100

    JavaFX init

---
+++ src/main/java/hu/unideb/inf/SpringDataFX/frontend/JavaFXApplication.java
@@ -0,0 +1,11 @@
+package hu.unideb.inf.SpringDataFX.frontend;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFXApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
